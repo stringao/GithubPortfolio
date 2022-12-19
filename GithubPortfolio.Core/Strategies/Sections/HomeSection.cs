025411f96f7859beafa111e5ae3ba80a15c4a0a8
Luiz Camargo <luiz.zito@hotmail.com>
2022-12-19 19:01:23 +0000

    Updates

--- GithubPortfolio.Core/Strategies/Sections/HomeSection.cs
+++ GithubPortfolio.Core/Strategies/Sections/HomeSection.cs
@@ -12,11 +12,13 @@ public class HomeSection : ISectionStrategy
     public string CreateContent(User user)
     {
         return $"""
-                <div class="section" id="{_id}" style="margin: 10rem 0; gap: 2rem; margin: 10rem 0 0;">
-                    <h2 class="text-primary" style="font-size: 3.5rem;"> HEY, I'M {user.Name.ToUpper()} </h2>
+                <div class="section" id="{_id}" style="margin: 10rem 0; gap: 2rem; margin: 10rem 0 0;">                   
                     <img src="{user.AvatarUrl}" alt="Profile Photo" class="circle"/>
+                    <h2 class="text-primary" style="font-size: 3.5rem;"> HEY, I'M {user.Name.ToUpper()} </h2>
                     <span style="font-size: 1.3rem;"> {user.Bio ?? _defaultBio} </span>                 
-                    <img src="./images/banner.svg" alt="Banner" class="banner"/>
+                    <img src="./images/banner.svg" alt="Banner" class="banner" 
+                    style="width: 55rem;
+                    max-width: 55rem;"/>
                 </div>  
                 """;
     }
